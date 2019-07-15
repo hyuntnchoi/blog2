d616c025ac75bdca2d54eaa1d101f831b1ec0dad
hyuntnchoi <hyuntn0724@gmail.com>
2019-07-15 22:53:35 +0900

    mongodb connected

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(process.env.MONGO_DB, {useNewUrlParser: true}, function(err, db){
+    if(err) throw err;
+    console.log("Database successfully connected!");
+    db.close();
+});
 
 app.get('/', function(req, res){
     res.send('hello world is boring');
