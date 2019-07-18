5a80a6cadb1628116028873069366094b692e1b0
hyuntnchoi <hyuntn0724@gmail.com>
2019-07-19 02:51:03 +0900

    done passport and User model

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
+var async = require('async');
+
 
 // connect database
 mongoose.connect(process.env.MONGO_DB2, {useNewUrlParser: true});
@@ -26,6 +31,14 @@ var postSchema = mongoose.Schema({
 });
 var Aaa = mongoose.model('bbb', postSchema);
 
+var userSchehma = mongoose.Schema({
+    email: {type:String, required:true, unique:true},
+    nickname: {type:String, required:true, unique:true},
+    password: {type:String, required:true},
+    createdAt: {type:Date, default:Date.now}
+});
+var Ccc = mongoose.model('ddd', userSchema);
+
 // view setting
 app.set("view engine", 'ejs');
 
@@ -34,8 +47,74 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json()); // 모든 서버에 도착하는 신호들의 body를 JSON으로 분석할 것
 app.use(bodyParser.urlencoded({extended:true})); // 위는 다른 프로그램이 json으로 데이터 전송을 할 경우 받는 body parser이고, 이건 웹사이트가 json으로 데이터를 전송할 경우 받는 body parser.
 app.use(methodOverride("_method"));
+app.use(flash());
 
-// set routes
+app.use(session({secret:'MySecret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(ddd, done){
+    done(null, ddd.id);
+});
+passport.deserializeUser(function(id, done){
+    Ccc.findById(id, function(err, user){
+        done(err, user);
+    });
+});
+
+var LocalStrategy = require('passport-local').Strategy;
+passport.use('local-login',
+  new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password', //email,password가 user스키마 안의 이름과 같아야함
+    passReqToCallback : true
+    },
+    function(req, email, password, done){
+        Ccc.findOne({ 'email' : email }, function(err, ddd){
+            if(err) return done(err);
+
+            if(!ddd){
+                req.flash("email", req.body.email);
+                return done(null, false, req.flash('loginError', 'No user found.'));
+            }
+            if(ddd.password != password){
+                req.flash("email", req.body.email);
+                return done(null, false, req.flash('loginError', 'Password does not Match'));
+            }
+            return done(null, user);
+        });
+    }
+  )
+);
+
+// set home routes
+app.get('/', function(req,res){
+    res.redirect('/posts');
+});
+app.get('/login', function(req, res){
+    res.render('login/login', {email:req.flash("email")[0], loginError:req.flash('loginError')});
+});
+app.post('/login',
+    function(req, res, next){
+        req.flash("email"); // flush email data
+        if(req.body.email.length === 0 || req.body.password.length === 0){
+            req.flash("email", req.body.email);
+            req.flash("loginError", "Please enter both email and password");
+            res.redirect('/login');
+        } else {
+            next();
+        }
+    }, passport.authenticate('local-login', {
+        successRedirect : '/posts',
+        failureRedirect : '/login',
+        failureFlash : true
+    })
+);
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
 app.get('/posts', function(req, res){
     Aaa.find({}).sort('-createdAt').exec(function(err, posts){
         if(err) return res.json({success:false, message:err});
